65e01c3e2421095a73d92412b43c9fc35b87714a
ranaslait <125487372+ranaslait@users.noreply.github.com>
2023-05-31 17:33:21 +0300

    404 and sessions

--- mongodb.js
+++ mongodb.js
@@ -2,8 +2,6 @@ const { string } = require("i/lib/util");
 const mongoose = require('mongoose');
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
-
-const bcryptSalt=10;
 const Schema = mongoose.Schema;
 
 const userschema= new Schema({
@@ -25,15 +23,5 @@ const userschema= new Schema({
     }
     
 });
-// userschema.methods.generatePasswordResetHash = function(){
-//     const resetHash = crypto.createHash('sha512').update(this.password).digest('hex')
-//     return resetHash;
-// }
-
-// //verify password reset hash
-// userschema.methods.verifyPasswordResetHash = function(resetHash = undefined){
-//     //regenerate hash and check if they are equal
-//     return this.passwordResetHash() === resetHash;
-// }
 const usercollection=new mongoose.model("user",userschema);
 module.exports=usercollection
