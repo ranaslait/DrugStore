d152a2899d041e5cc4286b4e6a26b243ca17a14b
mariam mahmoud <mariamsayed777@gmail.com>
2023-06-06 05:48:02 +0200

    index routes

--- routes/index.js
+++ routes/index.js
@@ -7,45 +7,84 @@ const Index=require("../controllers/Index");
 
 router.get("/",function(req,res){
    res.render('pages/index',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
+router.get("/index",function(req,res){
+   res.render('pages/index',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
 
-   });
+//nav routes
+router.get('/about', (req, res) => {
+   res.render('pages/about',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
+router.get('/blog', (req, res) => {
+   res.render('pages/blog',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
+router.get('/cart', (req, res) => {
+   res.render('pages/cart',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
+router.get('/checkout', (req, res) => {
+   res.render('pages/checkout',{user: (req.session.user === undefined ? "" : req.session.user) });
+});
 
+//mawgodeen fel user routes
+//login in & sign up routes
+// router.get('/login', (req, res) => {
+//    res.render('pages/login', { user: (req.session.user === undefined ? "" : req.session.user) });
+// });
+// router.get('/forgotPassword', (req, res) => {
+//    res.render('pages/forgotPassword');
+// });
+// router.get('/registr', (req, res) => {
+//    res.render('pages/registr', { user: (req.session.user === undefined ? "" : req.session.user) });
+// });
+// router.get('/load', (req, res) => {
+//    res.render('pages/load',{user: (req.session.user === undefined ? "" : req.session.user) });
+// });
+//-----------------------
 //    router.get('/srch', (req, res) => {
 //       res.render('pages/index',{user: (req.session.user === undefined ? "" : req.session.user) });
 //   });
 // router.post('/srch',srch1.srch);
 
-router.get("/index",function(req,res){
-res.render('pages/index',{user: (req.session.user === undefined ? "" : req.session.user) });
+//------------------------
+        //route articles yab2a gowa blog  
 
-});
- router.get('/about', (req, res) => {
-    res.render('pages/about',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/articles', (req, res) => {
-    res.render('pages/articles',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/blog', (req, res) => {
-    res.render('pages/blog',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
-router.get('/cart', (req, res) => {
-    res.render('pages/cart',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/cleaning', (req, res) => {
-    res.render('pages/cleaning',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/hair', (req, res) => {
-    res.render('pages/hair',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/login', (req, res) => {
-    res.render('pages/login', { user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/forgotPassword', (req, res) => {
-    res.render('pages/forgotPassword');
-});
- router.get('/registr', (req, res) => {
-    res.render('pages/registr', { user: (req.session.user === undefined ? "" : req.session.user) });
- });
+//  router.get('/articles', (req, res) => {
+//     res.render('pages/articles',{user: (req.session.user === undefined ? "" : req.session.user) });
+//  });
+ 
+//categories routes
+router.get('/medications', Index.GetAllProducts);
+router.get('/medications/productDetails/:id', Index.GetProduct);
+
+router.get('/vit', Index.GetAllProducts);
+router.get('/vit/productDetails/:id', Index.GetProduct);
+
+router.get('/personalcare', Index.GetAllProducts);
+router.get('/personalcare/productDetails/:id', Index.GetProduct);
+
+router.get('/makeup', Index.GetAllProducts);
+router.get('/makeup/productDetails/:id', Index.GetProduct);
+
+router.get('/skincare', Index.GetAllProducts);
+router.get('/skincare/productDetails/:id', Index.GetProduct);
+
+router.get('/cleaning', Index.GetAllProducts);
+router.get('/cleaning/productDetails/:id', Index.GetProduct);
+
+router.get('/motherbaby', Index.GetAllProducts);
+router.get('/motherbaby/productDetails/:id', Index.GetProduct);
+
+router.get('/medical', Index.GetAllProducts);
+router.get('/medical/productDetails/:id', Index.GetProduct);
+
+router.get('/hair', Index.GetAllProducts);
+router.get('/hair/productDetails/:id', Index.GetProduct);
+
+router.get('/perfume', Index.GetAllProducts);
+router.get('/perfume/productDetails/:id', Index.GetProduct);
+
+
 //  router.get('/adminsidebar', (req, res) => {
 //     res.render('pages/adminsidebar',{user: (req.session.user === undefined ? "" : req.session.user) });
 //  });
@@ -58,46 +97,19 @@ router.get('/cart', (req, res) => {
 //  router.get('/adminheader', (req, res) => {
 //     res.render('pages/adminheader',{user: (req.session.user === undefined ? "" : req.session.user) });
 //  });
- router.get('/404', (req, res) => {
-    res.render('pages/404',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
+
+   //-------------------------------
+//  router.get('/404', (req, res) => {
+//     res.render('pages/404',{user: (req.session.user === undefined ? "" : req.session.user) });
+//  });
+
  router.get('/footer', (req, res) => {
     res.render('pages/footer',{user: (req.session.user === undefined ? "" : req.session.user) });
  });
-router.get('/makeup', (req, res) => {
-    res.render('pages/makeup',{user: (req.session.user === undefined ? "" : req.session.user) });
+
+ //404 page
+ router.use((req, res) => {
+   res.render('pages/404',{user: (req.session.user === undefined ? "" : req.session.user) });
  });
- router.get('/medical', (req, res) => {
-    res.render('pages/medical',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/medications', Index.GetAllProducts);
- router.get('/medications/productDetails/:id', Index.GetProduct);
- router.get('/motherbaby', (req, res) => {
-    res.render('pages/motherbaby',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/perfume', (req, res) => {
-    res.render('pages/perfume',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/personalcare', (req, res) => {
-    res.render('pages/personalcare',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
-//  router.get('/productDetails', (req, res) => {
-//     res.render('pages/productDetails',{user: (req.session.user === undefined ? "" : req.session.user) });
-//  });
- router.get('/skincare', (req, res) => {
-    res.render('pages/skincare',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/vit', (req, res) => {
-    res.render('pages/vit',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/checkout', (req, res) => {
-    res.render('pages/checkout',{user: (req.session.user === undefined ? "" : req.session.user) });
- });
- router.get('/load', (req, res) => {
-   res.render('pages/load',{user: (req.session.user === undefined ? "" : req.session.user) });
-});
-//  router.get('/getFruits',(req,res)=>{
-//    let payload = req.body.payload.trim();
-//    console.log(payload);
-//     });
+
  module.exports=router
